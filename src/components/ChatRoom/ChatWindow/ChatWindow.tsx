e3c16a53de081baae651cb0a961804c593f44f24
Anh Do <anhdt.cntt@gmail.com>
2024-01-05 09:28:29 +0700

    Fix if no rooms have been selected

--- src/components/ChatRoom/ChatWindow/ChatWindow.tsx
+++ src/components/ChatRoom/ChatWindow/ChatWindow.tsx
@@ -6,7 +6,7 @@ import { AppContext } from "../../../Context/AppProvider";
 import Logo from "../../../assets/images/logo.png";
 
 export default function ChatWindow() {
-  const { selectedRoom } = useContext(AppContext);
+  const { selectedRoom, setTab } = useContext(AppContext);
 
   return (
     <div className="chat-window-wrapper">
@@ -18,7 +18,12 @@ export default function ChatWindow() {
       ) : (
         <div className="room-empty">
           <img src={Logo} alt="Sky Chat" className="logo-spin" />
-          <span>No rooms have been selected yet.</span>
+          <span>
+            No rooms have been selected yet. Please{" "}
+            <span className="choose-room" onClick={() => setTab(0)}>
+              choose room.
+            </span>
+          </span>
         </div>
       )}
     </div>
