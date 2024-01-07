963f3282743133077dd89d58acefcca02e6335f3
Anh Do <anhdt.cntt@gmail.com>
2024-01-07 15:54:12 +0700

    Fix re-render messages data

--- src/components/ChatRoom/ChatWindow/ChatWindowContent.tsx
+++ src/components/ChatRoom/ChatWindow/ChatWindowContent.tsx
@@ -85,7 +85,8 @@ export default function ChatWindowContent() {
 
   const messages: IMessage[] = useFirestore(
     collection.messages,
-    messagesCondition
+    messagesCondition,
+    1000
   );
 
   useEffect(() => {
